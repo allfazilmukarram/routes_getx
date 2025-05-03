33cbcfb5980bcfcbbffab198d280c1042a9e6a84
allfazilmukarram <alfazilmukarram162@gmail.com>
2025-05-04 00:33:11 +0700

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # route_management
 
 A new Flutter project.
+
+Cocok untuk latihan route manajegement
--- lib/pages/page_1.dart
+++ lib/pages/page_1.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 class PageSatu extends StatelessWidget {
   const PageSatu();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: SizedBox(),
-        title: Text("Page 1"),
-      ),
+      appBar: AppBar(leading: SizedBox(), title: Text("Page 1")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('Page 1'),
             Icon(Icons.import_contacts),
-            SizedBox(height: 20,),
-            ElevatedButton(onPressed: (){
-            // Navigator.of(context).pop();
-             Get.back();
-          }, child: Text('Back'), )
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigator.of(context).pop();
+                Get.back();
+              },
+              child: Text('Back'),
+            ),
           ],
         ),
       ),
